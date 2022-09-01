833432f64b6aa00ff3b72eba3decb37185d64dc6
shreyaCG <111388007+shreyaCG@users.noreply.github.com>
2022-09-01 18:44:47 +0530

    done

--- SocketPrg_V0.2/SocketPrg_V0.2/src/MainServer.cpp
+++ SocketPrg_V0.2/SocketPrg_V0.2/src/MainServer.cpp
@@ -2,25 +2,11 @@
 
 int main(int argc, char *argv[])
 {
-	int csockfd, client_sock[30];
-	
-	int max_clients = 30;
-
-	int serverfd, sd, max_sd;
-	
-	int readVal;
-
-	vector <int> vs_csock;
-	char buff[MAX_BUF];
-	//create master socket
-	fd_set master;
-	// set of sock_desc...
-	fd_set readfds;
-	int socketCount=0;
+	ServerSocket s1;
 
 	for(int i=0;i<max_clients;i++)
 		client_sock[i] = 0;
-
+	s1.serv_accept();
 	
 	return 0;
 }
